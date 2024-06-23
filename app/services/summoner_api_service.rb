f806b2f4fd9b29f417b8d0d73ed498cccbd74a42
Brandon Kounse <kounse@gmail.com>
2024-06-23 13:48:09 -0500

    updated class service to be a singleton with class methods

--- app/services/summoner_api_service.rb
+++ app/services/summoner_api_service.rb
@@ -2,12 +2,9 @@
 
 class SummonerApiService
   BASE_URL = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id'
+  @api_key = ENV['RIOT_LEAGUE_API_KEY']
 
-  def initialize
-    @api_key = ENV['RIOT_LEAGUE_API_KEY']
-  end
-
-  def fetch_summoner_PUUID(summoner_name, summoner_tag)
+  def self.fetch_summoner_PUUID(summoner_name, summoner_tag)
     response = RestClient.get("#{BASE_URL}/#{summoner_name}/#{summoner_tag}?api_key=#{@api_key}")
     data = JSON.parse(response.body)
     data['puuid']
