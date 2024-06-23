233db5a778d3a78161311e6523ab638cc71eb1c8
Brandon Kounse <kounse@gmail.com>
2024-06-23 12:09:13 -0500

    fixed bug in url preventing request from working

--- app/services/summoner_api_service.rb
+++ app/services/summoner_api_service.rb
@@ -1,7 +1,7 @@
 # Service object for interfacing with Riot Games API
 
 class SummonerApiService
-  BASE_URL = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'
+  BASE_URL = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id'
 
   def initialize
     @api_key = ENV['RIOT_LEAGUE_API_KEY']
@@ -9,7 +9,8 @@ class SummonerApiService
 
   def fetch_summoner_PUUID(summoner_name, summoner_tag)
     response = RestClient.get("#{BASE_URL}/#{summoner_name}/#{summoner_tag}?api_key=#{@api_key}")
-    JSON.parse(response.body['puuid'])
+    data = JSON.parse(response.body)
+    data['puuid']
   end
 
   private
