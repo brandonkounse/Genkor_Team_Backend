2b16d0f9c0404489170e31dc7d7c011f22866c34
Brandon Kounse <kounse@gmail.com>
2024-06-26 17:54:59 -0500

    defined team layout for use in backend api calls

---
+++ app/models/genkor_team.rb
@@ -0,0 +1,9 @@
+# Genkor teammates gameName mapped to their tagName
+
+TEAM = [
+  { "Kounse" => "NA1" },
+  { "InfernalStro" => "NA1" },
+  { "TheJimmyO" => "Jim" },
+  { "Benjix" => "NA1" },
+  { "TolkienBlack" => "TKB" }
+]
