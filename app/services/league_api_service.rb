da17657671190960cbd8783b36d0f143bf008525
Brandon Kounse <kounse@gmail.com>
2024-06-28 14:49:37 -0500

    added methods to fetch ranked stats

--- app/services/league_api_service.rb
+++ app/services/league_api_service.rb
@@ -6,6 +6,7 @@ class LeagueApiService
   API_KEY = ENV['RIOT_LEAGUE_API_KEY']
   ACCOUNT_V1 = ENV['ACCOUNT_V1_URL']
   SUMMONER_V4 = ENV['SUMMONER_V4_URL']
+  LEAGUE_V4 = ENV['LEAGUE_V4_URL']
 
   def get_team_profile_information(summoners)
     team = []
@@ -17,6 +18,12 @@ class LeagueApiService
       end
     end
     team
+  end
+
+  def get_summoner_ranked_stats(summoner_id)
+    response = RestClient.get("#{LEAGUE_V4}/#{summoner_id}?api_key=#{API_KEY}")
+    data = JSON.parse(response.body)
+    format_ranked_data(data)
   end
 
   private
@@ -40,5 +47,21 @@ class LeagueApiService
       summoner_level: data['summonerLevel'],
       puuid: data['puuid']
     }
+  end
+
+  def format_ranked_data(data)
+    formatted_data = {}
+    data.each do |obj|
+      stats = {}
+      stats["leagueId"] = obj["leagueId"]
+      stats["queueType"] = obj["queueType"]
+      stats["tier"] = obj["tier"]
+      stats["rank"] = obj["rank"]
+      stats["wins"] = obj["wins"]
+      stats["losses"] = obj["losses"]
+      stats["summonerId"] = obj["summonerId"]
+      formatted_data[obj["queueType"]] = stats
+    end
+    formatted_data
   end
 end
