62c859c4f62a9c5cd420f761f6ee17b5a38b6a7d
Brandon Kounse <kounse@gmail.com>
2024-06-28 14:48:47 -0500

    added error handle as separate method and various helper functions for database retrieval

--- app/services/firebase_service.rb
+++ app/services/firebase_service.rb
@@ -15,25 +15,52 @@ class FirebaseService
     response = RestClient.get(url)
     JSON.parse(response.body)
   rescue RestClient::ExceptionWithResponse => e
-    puts "Error: #{e.response}"
-    nil
+    handle_http_error(e)
+  end
+
+  def get_all_team_member_data
+    url = "#{BASE_URL}/.json?auth=#{@id_token}"
+    response = RestClient.get(url)
+    JSON.parse(response.body)
+  rescue RestClient::ExceptionWithResponse => e
+    handle_http_error(e)
+  end
+  
+  def get_all_team_members_names
+    names = []
+    
+    get_all_team_member_data.each_key do |k|
+      names.push(k)
+    end
+    names
+  end
+
+  def get_all_team_member_summoner_ids
+    ids = []
+
+    get_all_team_member_data.each do |key, value|
+      ids.push(value['profile_info']['summoner_id'])
+    end
+    ids
   end
 
   def store_team_member_data(summoner_name, summoner_data)
     url = "#{BASE_URL}/#{summoner_name}.json?auth=#{@id_token}"
     payload = summoner_data.to_json
     response = RestClient.put(url, payload, headers)
-
     JSON.parse(response.body)
   rescue RestClient::ExceptionWithResponse => e
-    puts "Error: #{e.response}"
-    nil
+    handle_http_error(e)
   end
 
   private
 
   def headers
     { content_type: :json, accept: :json }
+  end
+
+  def handle_http_error(e)
+    puts "Error: #{e.response}"
   end
 
   def get_id_token
@@ -48,7 +75,6 @@ class FirebaseService
 
     JSON.parse(response.body)['idToken']
   rescue RestClient::ExceptionWithResponse => e
-    puts "Error: #{e.response}"
-    nil
+    handle_http_error(e)
   end
 end
